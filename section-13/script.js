400c5becfa1449c3aaa9ab9520f1a14dc0675791
zalan kovacs <zalan.dev@gmail.com>
2023-08-28 00:33:48 +0300

    mesage

--- section-13/script.js
+++ section-13/script.js
@@ -107,25 +107,26 @@ tabsContainer.addEventListener('click', function (e) {
   clicked.classList.add('operations__tab--active');
 
   // Activate content area
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
-
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 // Menu fade animation
 const handleHover = function (e) {
   // console.log(this, e.currentTarget);
 
-  if(e.target.classList.contains('nav__link')){
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if(el !== link) el.style.opacity = this;
+      if (el !== link) el.style.opacity = this;
     });
     logo.style.opacity = this;
   }
-}
+};
 
 // Passing "argument into handler"
 nav.addEventListener('mouseover', handleHover.bind(0.5));
@@ -163,17 +164,17 @@ window.addEventListener('scroll', function(){
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 
-const stickyNav = function(entries){
+const stickyNav = function (entries) {
   const [entry] = entries;
   // console.log(entry);
 
-  if(!entry.isIntersecting) nav.classList.add('sticky');
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
-}
+};
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: 0, 
+  threshold: 0,
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
@@ -181,52 +182,139 @@ headerObserver.observe(header);
 // Reveal(felfed) sections
 const allSections = document.querySelectorAll('.section');
 
-const revealSection = function(entries, observer){
+const revealSection = function (entries, observer) {
   const [entry] = entries;
   // console.log(entry);
 
-  if(!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
 
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
-  
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
 });
-allSections.forEach(function(section){
+allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // Lazy loading images
 const imgTargets = document.querySelectorAll('img[data-src]');
-console.log(imgTargets);
 
-const loadImg = function(entries, observer){
+const loadImg = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
-  if(!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
 
   // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
-  
-  entry.target.addEventListener('load', function(){
+
+  entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
 
     observer.unobserve(entry.target);
-  })
+  });
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '-200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        //a tartalmat bassza bele
+        'beforeend', //itt mondja meg hogy hova
+        `<button class="dots__dot" data-slide=${i}></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+
+  init();
+
+  // Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    console.log(e);
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide(); //ezzel egyenlo: if(e.key === 'ArrowRight') nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset; //=== slide = e.target.dataset.slide;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+
+slider();
 
 //////////////////
 //////////////////
@@ -389,3 +477,16 @@ console.log(h1.parentElement.children);
   if(el !== h1) el.style.transform = 'scale(0.5)';
 });
 */
+document.addEventListener('DOMContentLoaded', function(e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function(e) {
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', function(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = 'message';
+// })
