bd9cbaa4324291aad9927263d6310b7a6bddcd96
zalan kovacs <zalan.dev@gmail.com>
2023-09-01 00:23:05 +0300

    mesage

--- section-14/script.js
+++ section-14/script.js
@@ -1,1 +1,201 @@
 'use strict';
+
+const Person = function(firstName, birtYear) {
+    // Istance properties
+    this.firstName = firstName;
+    this.birtYear = birtYear;
+
+    // Never to this
+    // this.calcAge = function {
+    //     console.log(2037 - this.birtYear);
+    // }
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype 
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(jonas instanceof Person);
+
+Person.hey = function() {
+    console.log('Hey there 👏');
+    console.log(this);
+}
+
+Person.hey();
+
+/////////////////////
+// Prototypes
+console.log(Person.prototype,   11111111111);
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birtYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function() {
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x++);
+
+
+/////////////////
+// Coding Challenge #1
+/*
+const Car = function(make, speed){
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function(){
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+Car.prototype.brake = function(){
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+*/
+
+// Class expression
+// const PersonCl = class {};
+
+// Class declaration
+class PersonCl {
+    constructor(fullName, birthYear){
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Instance methods
+    // Methods will be added to .prototype property
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    }
+
+    greet(){
+        console.log(`Hey ${this.firstName}`);
+    }
+
+    get age(){
+        return 2037 - this.birthYear;
+    }
+
+    // Set a property that already exist
+    set fullName(name){
+        console.log(name);
+        if(name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName(){
+        return this._fullName;
+    }
+
+    // Statoc method
+    static hey() {
+        console.log('Hey there 👏');
+        console.log(this);
+    }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function(){
+    console.log(`Hey ${this.firstName}`);
+}
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+PersonCl.hey();
+
+const account = {
+    owner: 'jonas',
+    movements:  [200, 530, 120 ,300],
+
+    get latest(){
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov){
+        this.movements.push(mov);
+    }
+}
+
+console.log(account.latest);
+// console.log(account.movements);
+
+account.latest = 50;
+console.log(account.movements);
+
+const PersonProto = {
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
